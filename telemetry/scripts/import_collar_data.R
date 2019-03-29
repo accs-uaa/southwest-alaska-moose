aac405a2670596e9a53a95f58d75570a837e2535
Amanda Droghini <adroghini@alaska.edu>
2019-03-28 17:10:09 -0800

    Update import_collar_data.R
    Finish merge of GPS & VHF data into a single file

--- telemetry/scripts/import_collar_data.R
+++ telemetry/scripts/import_collar_data.R
@@ -1,5 +1,11 @@
+# Objective: Import data from GPS and VHF collars. Combine all data files into a single dataframe that can be used for analyses.
+# Author: A. Droghini (adroghini@alaska.edu), Alaska Center for Conservation Science
+# Last updated: 28 March 2019
+
 # Load required packages
+library(plyr)
 library(tidyverse)
+library(readxl)
 
 # Read in moose collar data
 # Data were downloaded from Vectronic on 22 Mar 2019
@@ -44,14 +50,48 @@ attach(telem.data)
 
 # List of changes to make to dataframe:
 # See https://www.vectronic-aerospace.com/wp-content/uploads/2016/04/Manual_GPS-Plus-X_v1.2.1.pdf) page 125 for meaning of column names
+
 # Drop the following columns: "Origin", "Activity", "Sats", "X3D_Error..m."
 # Columns only have the same value across all rows
-# Drop "Main..V." and "Beacon..V." - unnecessary for analyses, tells you about collar battery life 
-# Drop SCTS Date/time - this is note the date/time of the fix
+# Drop "Main..V.", "Beacon..V." - unnecessary for analyses, tells you about collar battery life 
+# Drop "SCTS_Date", "SCTS_Time" - this is note the date/time of the fix
 
 # Code CollarID and AnimalID as factors
 # Rename Latitude..Â.. ; Longitude..Â.. ; Temp..Â.C. ; Height..m.
 
+# Add column to differentiate between VHF & GPS data
 
+drop.cols <- c("Origin", "Activity", "Sats", "X3D_Error..m.", "Main..V.", "Beacon..V.",
+               "SCTS_Date", "SCTS_Time")
 
+telem.data <- telem.data %>% 
+  select(-drop.cols) %>% 
+  rename(Long_X = "Longitude..Â..", Lat_Y = "Latitude..Â..", Temp_C = "Temp..Â.C.",
+         Height_m = "Height..m.") %>% 
+  mutate(CollarID=as.factor(CollarID),
+         AnimalID=as.factor(AnimalID),
+         Collar_Type = "GPS")
+  
+rm(drop.cols)
+
+# Load VHF data
+vhf.data <- read_excel("collar_data/vhf_moose_data.xlsx")
+
+names(vhf.data)
+# For now, keep only "Flight_Date", "Moose_ID", "Lat_DD", "Lon_DD"
+# Add column to differentiate between VHF & GPS data
+# Rename columns to match with telem.data
+# Get rid of entries with no lat or lon
+
+vhf.data <- vhf.data %>% 
+  select("Flight_Date", "Moose_ID", "Lat_DD", "Lon_DD") %>% 
+  rename(Long_X = "Lon_DD", Lat_Y = "Lat_DD", AnimalID = Moose_ID,
+         LMT_Date = Flight_Date) %>% 
+  mutate(Collar_Type = "VHF") %>% 
+  filter(!is.na(Lat_Y) | !is.na(Long_X))
+
+# Final merge
+telem.data <- rbind.fill(telem.data,vhf.data)
+
+rm(vhf.data)
 
