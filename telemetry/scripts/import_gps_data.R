b001e76b8dccdf3af94de8327260cd18095395fb
Amanda Droghini <adroghini@alaska.edu>
2020-01-29 09:18:26 -0900

    Update import_gps_data.R
    change file names to camelCase

--- telemetry/scripts/import_gps_data.R
+++ telemetry/scripts/import_gps_data.R
@@ -8,19 +8,18 @@
 # Load packages and data files----
 # Data were downloaded from Vectronic on 5 Nov 2019
 # Data for each moose are stored as separate .csv files
-
 library(plyr)
 library(tidyverse)
 
-data.files <-list.files(file.path('collar_data/gps'),full.names = TRUE,pattern=".csv")
+dataFiles <-list.files(file.path('collar_data/gps'),full.names = TRUE,pattern=".csv")
 
 # Read in each file and combine into single dataframe
 # "No" column is not unique across all individuals, but is unique within each individual
 # Sorted in decreasing order? (newest to oldest)
 # Change "No" column so that numbers go from oldest to newest. Rename as RowID
 
-for (i in 1:length(data.files)) {
-  f <- data.files[i]
+for (i in 1:length(dataFiles)) {
+  f <- dataFiles[i]
   
   temp <- read.csv(f,stringsAsFactors = FALSE)
   # print(max(temp$No)+1==length(temp$No))
@@ -31,20 +30,20 @@ for (i in 1:length(data.files)) {
   rename(RowID = No)
   
   if (i == 1) {
-    gps.data <- temp
+    gpsData <- temp
     
   } else {
-    gps.data <- rbind(gps.data, temp)
+    gpsData <- rbind(gpsData, temp)
   }
 }
 
 # Clean workspace
-rm(f,i,temp,data.files)
+rm(f,i,temp,dataFiles)
 
 # Remove extra columns----
-names(gps.data)
-summary(gps.data)
-# length(unique(gps.data$CollarID))
+names(gpsData)
+summary(gpsData)
+# length(unique(gpsData$CollarID))
 # unique(X3D_Error..m.)
 
 
@@ -67,21 +66,21 @@ summary(gps.data)
 
 # Add column to differentiate between VHF & GPS data
 
-drop.cols <- c("Activity", "X3D_Error..m.", "Main..V.", "Beacon..V.",
-               "SCTS_Date", "SCTS_Time",names(gps.data)[19:43],"AnimalID","GroupID",
+dropCols <- c("Activity", "X3D_Error..m.", "Main..V.", "Beacon..V.",
+               "SCTS_Date", "SCTS_Time",names(gpsData)[19:43],"AnimalID","GroupID",
                "ECEF_X..m.","ECEF_Y..m.","ECEF_Z..m.")
 
-gps.data <- gps.data %>% 
-  select(-drop.cols) %>% 
+gpsData <- gpsData %>% 
+  select(-dropCols) %>% 
   rename(Long_X = "Longitude....", Lat_Y = "Latitude....", Temp_C = "Temp...C.",
          Height_m = "Height..m.") 
 
 # Code CollarID as factor
-gps.data <- gps.data %>% 
-  mutate(CollarID=as.factor(CollarID))
-  
-rm(drop.cols)
+# gpsData <- gpsData %>% 
+  # mutate(CollarID=as.factor(CollarID))
 
 #### Export----
-save(gps.data, file="output/gps_raw.Rdata")
+save(gpsData, file="output/gps_raw.Rdata")
 
+# Clean workspace
+rm(dropCols,gpsData)
