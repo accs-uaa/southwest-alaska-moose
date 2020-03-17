7f566d3e71311f70b916bad1770dcf331a4bcfb8
Amanda Droghini <adroghini@alaska.edu>
2020-03-17 12:44:41 -0800

    Update function-subsetIDTimeLags.R
    Added evaluation of time steps less than a specified threshold

--- telemetry/scripts/function-subsetIDTimeLags.R
+++ telemetry/scripts/function-subsetIDTimeLags.R
@@ -1,12 +1,14 @@
-# This function takes a moose ID and a timeStep. It returns a dataframe that includes data for only the specified unit. It also prints row indices for which the fix rate is greater than the threshold specified (timeStep). Time units of timeStep should match units of date/time column in dataframe)
+# This function takes a moose ID and a timeStep. It returns a dataframe that includes data for only the specified unit. It also prints row indices for which the fix rate is less than or greater than the threshold specified (timeStep). Time units of timeStep should match units of date/time column in dataframe)
 
-subsetTimeLags <- function(id,timeStep) {
+subsetTimeLags <- function(id,minTimeStep,maxTimeStep) {
   n = which(ids==id)
   subsetID <- gpsMove[[n]]
   subsetID <- subsetID@data
   timeLag <- timeLags[[n]]
-  cat("Evaluating...",id,'\n',"Row indices with time step greater than",timeStep,":")
-  print(which(timeLag>timeStep)) 
+  cat("Evaluating...",id,'\n',"Row indices with time step lessthan",minTimeStep,":")
+  print(which(timeLag<minTimeStep)) 
+  cat("Row indices with time step greater than",maxTimeStep,":")
+  print(which(timeLag>maxTimeStep)) 
   return(subsetID)
 }
 
