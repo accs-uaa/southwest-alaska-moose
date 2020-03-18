c6b5b7b894d1e5f0458f4006fc218732766518fd
Amanda Droghini <adroghini@alaska.edu>
2020-03-17 17:00:55 -0800

    Update function-subsetIDTimeLags.R
    Added optional parameter stepLengths

--- telemetry/scripts/function-subsetIDTimeLags.R
+++ telemetry/scripts/function-subsetIDTimeLags.R
@@ -1,6 +1,9 @@
 # This function takes a moose ID and a timeStep. It returns a dataframe that includes data for only the specified unit. It also prints row indices for which the fix rate is less than or greater than the threshold specified (timeStep). Time units of timeStep should match units of date/time column in dataframe)
 
-subsetTimeLags <- function(id,minTimeStep,maxTimeStep) {
+# Step lengths can be calculated by specifying stepLengths = TRUE
+
+subsetTimeLags <- function(id,minTimeStep,maxTimeStep,stepLengths = NULL) {
+  require(tidyverse)
   n = which(ids==id)
   subsetID <- gpsMove[[n]]
   subsetID <- subsetID@data
@@ -9,6 +12,14 @@ subsetTimeLags <- function(id,minTimeStep,maxTimeStep) {
   print(which(timeLag<minTimeStep)) 
   cat("Row indices with time step greater than",maxTimeStep,":")
   print(which(timeLag>maxTimeStep)) 
+ 
+  if(!is.null(stepLengths)) 
+    subsetID <- subsetID %>% 
+    dplyr::mutate(z=Easting+1i*Northing, 
+                  steps=c(NA,diff(z)), #NA for 1st step length
+                  steplength_m=Mod(steps)) %>%
+    dplyr::select(-c(z,steps))
+  
   return(subsetID)
 }
 
