2965dee534330b19a744e42a1a1f2d7463ba8a34
Amanda Droghini <adroghini@alaska.edu>
2020-03-17 17:01:52 -0800

    Update 02-remove_outliers.R
    Went through identifying time lag outliers all over again + use of timeLag function

--- telemetry/scripts/02-remove_outliers.R
+++ telemetry/scripts/02-remove_outliers.R
@@ -1,55 +1,62 @@
+# Objective: Examine GPS data for issues: outliers, duplicates, and skips.
 
+# Author: A. Droghini (adroghini@alaska.edu)
+#         Alaska Center for Conservation Science
 
-#### Explore data for outliers----
+#### Load data and packages----
+rm(list = ls())
 
+library(tidyverse)
+library(move)
+library(ctmm)
 
-plot(gpsData$Long_X, gpsData$Lat_Y, xlab="Longitude", ylab="Latitude")
-
-# Remove outliers with long values in the 13s, which is in Germany where collars are manufactured
-
-gpsData <- gpsData %>% 
-  filter(Long_X < -152)
-
-plot(gpsData$Long_X, gpsData$Lat_Y, xlab="Longitude", ylab="Latitude")
-
-summary(gpsData) # no NAs for Lat, Long
+load("output/gps_clean_all.Rdata")
+source("scripts/function-subsetIDTimeLags.R")
 
 #### Convert to Movebank object----
-# According to Vectronic manual (https://vectronic-aerospace.com/wp-content/uploads/2016/04/Manual_GPS-Plus-Collar-Manager-V3.11.3.pdf), Lat/Long is in WGS84. I can't find any information on Easting/Northing column, so using Lat/Long for now
-names(gpsMove)
+# According to Vectronic manual (https://vectronic-aerospace.com/wp-content/uploads/2016/04/Manual_GPS-Plus-Collar-Manager-V3.11.3.pdf), Lat/Long is in WGS84. 
+# Required for calculating timeLags
 
-gpsMove <- move(x=gpsData$Long_X,y=gpsData$Lat_Y,
+gpsMove <- move(x=gpsData$longX,y=gpsData$latY,
                 time=gpsData$datetime,
                 data=gpsData,proj=CRS("+proj=longlat +ellps=WGS84"),
-                animal=gpsData$CollarID, sensor="gps")
+                animal=gpsData$deployment_id, sensor="gps")
 
-show(gpsMove)
-show(split(gpsMove))
+# show(gpsMove)
+# show(split(gpsMove))
 n.indiv(gpsMove) # no of individuals
 n.locs(gpsMove) # no of locations per individuals
 
-#### Check for additional outliers----
+# Explore data for outliers----
 # See vignette: https://ctmm-initiative.github.io/ctmm/articles/error.html
 # This is always so hacky & iterative, and I never have a good workflow for this
 # 
-# 1. Check time lags. A lot of the times the problems stem for unidentified redeployments or collar issues at the start/end of deployment
-# 1. Check loctaion outliers. 
-# 2. Investigate outliers manually. Consider: 1- Missed fixes, 2- Impossible speeds or distances, 3- Impossible locations
+# 1. Check location outliers. 
+# 2. Check time lags. Fix rate is 2 hours- find instances where fix rate is much smaller/larger. A lot of the times the problems stem from unidentified redeployments (though this should have been addressed in previous script) or collar issues at the start/end of deployment
+# 3. Investigate outliers manually. Consider: 1- Missed fixes, 2- Impossible speeds or distances, 3- Impossible locations
 
-ctData <- as.telemetry(gpsMove)
-ids <- names(ctData)
+# Location outliers----
 
-# Time lags between locations
-timeLags <- timeLag(gpsMove, units='hours')
-# Fix rate is 2 hours
+plot(gpsData$longX, gpsData$latY, xlab="Longitude", ylab="Latitude")
 
-# Print summaries & plots
+summary(gpsData) 
+
+# Will tackle other, less noticeable location outliers on a case-by-case basis later
+
+#### Check for outlying fix rates----
+
+# Calculate time lags between locations
+timeLags <- move::timeLag(gpsMove, units='hours')
+ids <- unique(gpsData$deployment_id)
+
+# Generate plots and quantitative summary
 lagSummary <- data.frame(row.names = ids)
 
 for (i in 1:length(ids)){
   timeL <- timeLags[[i]]
-  lagSummary$min[i] <- min(timeL)
   
+  lagSummary$id[i] <- ids[i]
+  lagSummary$min[i] <- min(timeL)
   lagSummary$mean[i] <- mean(timeL)
   lagSummary$sd[i] <- sd(timeL)
   lagSummary$max[i] <- max(timeL)
@@ -63,150 +70,155 @@ for (i in 1:length(ids)){
   
 }
 
-rm(plotName,filePath,finalName,i)
+rm(plotName,filePath,finalName,i,timeL)
 
 # Collars with time lag issues----
-subset(lagSummary, max > 2.05)$ids
 
-# M30927 & M30928 were redeployed. Need to recode (done at a later step).
+# Basically all of them have problems.
 
-# investigating... 103
-which(ids=="M30103")
-id <- gpsMove[[2]]
-id <- id@data
-timeL <- timeLags[[2]]
-which(timeL>2.05)
+# min goes from 1.95 to 0.44 and max goes from 2.05 to 3.99, so thresholds don't matter too much
 
-View(id[1:20,]) # 1. Solution: Delete start (n=6). Time lags are erratic for the first few days.
-View(id[970:985,]) # No data on 28 Jun 2018 but doesn't seemed to have moved much. Can we apply Kalman filtering?
-test <- id[960:985,]
-plot(test$Long_X, test$Lat_Y, type="b") 
-rm(test)
+# investigating... M30102
+subsetID <- subsetTimeLags("M30102",1.95,2.05) # nothing, good to go.
 
-# investigating... 104
-which(ids=="M30104")
-id <- gpsMove[[3]]
-id <- id@data
-timeL <- timeLags[[3]]
-which(timeL>2.05)
-View(id[965:975,]) # Same issue as M30103. Conspiracy!
+# investigating... M30103
+subsetID <- subsetTimeLags("M30103",1.95,2.05)
 
-# investigating... 105
-id <- gpsMove[[4]]
-id <- id@data
-timeL <- timeLags[[4]]
-which(timeL>2.05)
-View(id[1:10,]) # 1. Solution: Delete start (n=5).
-View(id[1704:1715,]) #2. No data for 3AM fix on 8/26/2018. Need to solve*
+View(subsetID[1:10,]) # 1. Collar starts on April 5 2018, but fix rates do not become consistent until April 8 (barely any data from 6-7 Apr). Solution: Delete start (n=6).
+View(subsetID[970:985,]) # No data on 28 Jun 2018 but doesn't seemed to have moved much. Can we apply Kalman filtering?
+
+# investigating... M30104
+subsetID <- subsetTimeLags("M30104",1.95,2.05)
+View(subsetID[965:975,]) # Same issue as M30103- no data on 28 June 2018. Conspiracy! Will have to resolve at later date.
+
+# investigating... M30105
+subsetID <- subsetTimeLags("M30105",1.95,2.05)
+View(subsetID[1:10,]) # 1. Collar starts on 5 April 2018, but fixes do not become consistent until 6 April (no data from 15:00 5 April to 3:00 6 April). Solution: Delete start (n=5).
+View(subsetID[1704:1715,]) #2. No data for 3AM fix on 8/26/2018. Need to interpolate*
 
 # investigating... M30894
-id <- gpsMove[[5]]
-id <- id@data
-timeL <- timeLags[[5]]
-which(timeL>2.05)
-View(id[1:15,]) # 1. Solution: Delete start (n=8).
-View(id[1315:1345,]) #2. Missing two fixes at 11AM on two consecutive days. Need to solve*
-View(id[6900:6910,]) # Missing a couple of other fixes- should be able to interpolate.
+subsetID <- subsetTimeLags("M30894",1.95,2.05)
+
+View(subsetID[1:15,]) # 1. Collar starts on 5 April 2018, but fixes do not become consistent until 6 April. Solution: Delete start (n=8).
+View(subsetID[1315:1345,]) #2. Missing two fixes at 11AM on 24-25 July 2018. Need to interpolate*
+View(subsetID[1995:2005,]) # Missing a fix at 7AM 19 September 2018
+View(subsetID[3120:3137,]) # Missing a fix at 1AM 2018-12-22
+View(subsetID[4650:4660,]) # Missing a fix at 19:00 2019-04-28
+View(id[5827:5833,]) # Missing one whole day (5 Aug 2019). Fix goes from 2019-08-04 15:00:29 to 2019-08-06 17:00:19
+
+# investigating... M30926
+subsetID <- subsetTimeLags("M30926",1.95,2.05)
+View(subsetID[1:10,]) # Fix jumps from 2018-04-05 15:00:42 to 2018-04-05 23:00:15. Solution: Delete start (n=5)
+View(subsetID[4855:4930,]) # Datasheet said individual died on 15-05-2019. Stays stationary several days before now. Last time to include will be 2019-05-12 09:00:39 (Row ID 4823). This is the first instance where the moose travels to lon 58.82828	lat -158.0130. All points after that jump around this area.
+
+test <- subset(subsetID,RowID>4800&RowID<4824)
+plot(test$longX,test$latY,type="b")
+rm(test)
+
+# investigating... M30927a
+# this is one of our individuals that died. date of death on datasheet is indicated as 22-05-2018, but may be earlier
+# Calculate step lengths distances
+subsetID <- subsetTimeLags("M30927a",1.95,2.05,stepLengths=TRUE) # no missed fixes identified
+
+# No obvious point at which animal stops moving so don't remove anything off the end for now
+
+# investigating... M30927b
+subsetID <- subsetTimeLags("M30927b",1.95,2.05)
+View(subsetID[1533:1540,]) # Jumps from 2019-08-06 19:00:43 to 2019-08-07 21:00:29. Need to interpolate*
+
+# investigating... M30928a
+# death date on datasheet written as 20-05-2018
+subsetID <- subsetTimeLags("M30928a",1.95,2.05,stepLengths=TRUE) 
+View(subsetID[1:8,]) # Jumps from 2018-04-05 15:00:39 to 2018-04-06 05:00:16. Solution: Delete start (n=4)
+# Get rid of every record with RowID>519. Arbitrary -- Can revisit later
+
+
+# investigating... M30928b
+subsetID <- subsetTimeLags("M30928b",1.95,2.05)
+View(subsetID[1108:1112,]) 
+View(subsetID[1495:1500,]) # Random missed fixes
 
 # investigating... M30930
-which(ids=="M30930")
-id <- gpsMove[[9]]
-id <- id@data
-timeL <- timeLags[[9]]
-which(timeL>2.05) # Missing a couple of non-consecutive fixes- should be able to interpolate.
+subsetID <- subsetTimeLags("M30930",1.95,2.05)
+# Missing a couple of non-consecutive fixes- should be able to interpolate.
 
 # investigating... M30931
-id <- gpsMove[[10]]
-id <- id@data
-timeL <- timeLags[[10]]
-which(timeL>2.05) # Missing a couple of non-consecutive fixes- should be able to interpolate.
-View(id[1:10,]) # 1. Solution: Delete start (n=5).
+subsetID <- subsetTimeLags("M30931",1.95,2.05)
+View(subsetID[1:7,]) # 1. Solution: Delete start (n=5).
+# Missing a couple of other non-consecutive fixes- should be able to interpolate.
 
 # investigating... M30932
-id <- gpsMove[[11]]
-id <- id@data
-timeL <- timeLags[[11]]
-which(timeL>2.05) # Missing a couple of non-consecutive fixes- should be able to interpolate.
+subsetID <- subsetTimeLags("M30932",1.95,2.05)
+# Missing a couple of non-consecutive fixes- should be able to interpolate.
 
 # investigating... M30933
-id <- gpsMove[[12]]
-id <- id@data
-timeL <- timeLags[[12]]
-which(timeL>2.05) # Only missing one
+subsetID <- subsetTimeLags("M30933",1.95,2.05)
+# Only missing one
+
+# investigating... M30934
+subsetID <- subsetTimeLags("M30934",1.95,2.05) # good to go
 
 # investigating... M30935
-id <- gpsMove[[14]]
-id <- id@data
-timeL <- timeLags[[14]]
-which(timeL>2.05) 
-View(id[1:10,]) # 1. Solution: Delete start (n=5). Only missing one more.
+subsetID <- subsetTimeLags("M30935",1.95,2.05)
+View(subsetID[1:7,]) # 1. Solution: Delete start (n=5).
+
+# investigating... M30936
+subsetID <- subsetTimeLags("M30936",1.95,2.05)
+View(subsetID[1:10,]) # 1. Delete start (n=8). 
 
 # investigating... M30937
-id <- gpsMove[[15]]
-id <- id@data
-timeL <- timeLags[[15]]
-which(timeL>2.05) 
-View(id[1:12,]) # 1. Delete start (n=7). Only missing three more scattered throughout data set.
+subsetID <- subsetTimeLags("M30937",1.95,2.05)
+View(subsetID[1:12,]) # 1. Delete start (n=7). Only missing three more scattered throughout data set.
 
 # investigating... M30938
-id <- gpsMove[[16]]
-id <- id@data
-timeL <- timeLags[[16]]
-which(timeL>2.05) # Missing a couple of non-consecutive fixes
-View(id[6650:6670,]) 
+subsetID <- subsetTimeLags("M30938",1.95,2.05) # good to go
 
-# investigating... M309389
-id <- gpsMove[[17]]
-id <- id@data
-timeL <- timeLags[[17]]
-which(timeL>2.05) # Missing a couple of non-consecutive fixes
+# investigating... M30939
+subsetID <- subsetTimeLags("M30939",1.95,2.05) # Missing a couple of non-consecutive fixes
 
 # investigating... M30940
-id <- gpsMove[[18]]
-id <- id@data
-timeL <- timeLags[[18]]
-which(timeL>2.05) # 1. Delete start (n=7). Only missing two more. 	
-View(id[1:12,])
+subsetID <- subsetTimeLags("M30940",1.95,2.05)
+View(subsetID[1:12,]) # 1. Delete start (n=7). Only missing two more. 
 
 # investigating... M35172
-id <- gpsMove[[19]]
-id <- id@data
-timeL <- timeLags[[19]]
-which(timeL>2.05) # Missing a couple of non-consecutive fixes	
+subsetID <- subsetTimeLags("M35172",1.95,2.05)
+which(timeL>2.05) # Only missing one fix
 
-# Total number of rows deleted: 6 + 5 + 8 +5 +5 +7 +7 = 43
-
-# Make changes to gpsData for easier filtering & then re-convert to move object
-
-gpsClean <- gpsData %>% 
-  filter(!(CollarID == "M30103" & RowID < 144 | CollarID == "M30105" & RowID < 129 |
-             CollarID == "M30894" & RowID < 112 | CollarID == "M30931" & RowID < 109 | 
-             CollarID == "M30935" & RowID < 109 | CollarID == "M30937" & RowID < 111 | 
-             CollarID == "M30940" & RowID < 112))
-
-# 43 rows less than gpsData
-
-# Convert gpsClean to move object
-gpsMove <- move(x=gpsClean$Long_X,y=gpsClean$Lat_Y,
-                time=gpsClean$datetime,
-                data=gpsClean,proj=CRS("+proj=longlat +ellps=WGS84"),
-                animal=gpsClean$CollarID, sensor="gps")
-ctData <- as.telemetry(gpsMove)
-ids <- names(ctData)
+# investigating... M35173
+subsetID <- subsetTimeLags("M35173",1.95,2.05) # good to go
 
 # Workspace clean-up
-rm(id,timeLags,timeL,lagSummary)
+rm(ids,timeLags,lagSummary,subsetID,subsetTimeLags)
+
+gpsClean <- gpsData %>% 
+  filter(!(deployment_id == "M30103" & RowID <= 6 | deployment_id == "M30105" & RowID <= 5 | 
+             deployment_id == "M30894" & RowID <= 8 | deployment_id == "M30926" & RowID <= 5 |
+             deployment_id == "M30926" & RowID > 4823 | deployment_id == "M30928a" & RowID <= 4 |
+             deployment_id == "M30928a" & RowID > 519 |
+             deployment_id == "M30931" & RowID <= 5 | deployment_id == "M30935" & RowID <= 5 | 
+             deployment_id == "M30936" & RowID <= 8 | deployment_id == "M30937" & RowID <= 7 |
+             deployment_id == "M30940" & RowID <= 7))
+
+# Total number of rows deleted: 293 (~0.3%)
+
+# Convert gpsClean to move object
+gpsMove <- move(x=gpsClean$longX,y=gpsClean$latY,
+                time=gpsClean$datetime,
+                data=gpsClean,proj=CRS("+proj=longlat +ellps=WGS84"),
+                animal=gpsClean$deployment_id, sensor="gps")
 
 #### Investigate location outliers----
 # Using the ctmm::outlie function
 # Generate ctmm::outlie plots for each individual
 # High-speed segments are in blue, while distant locations are in red
 
-# Grab some coffee in the break room while this runs
+ctmmData <- as.telemetry(gpsMove)
+ids <- names(ctmmData)
 ctmmSummary <- data.frame(row.names = ids)
+# Grab some coffee in the break room while this runs
 
 for (i in 1:length(ids)){
-  out <- outlie(ctData[[i]],plot=TRUE,main=ids[i])
+  out <- outlie(ctmmData[[i]],plot=TRUE,main=ids[i])
   ctmmSummary$minSpeed[i] <- min(out$speed)
   ctmmSummary$meanSpeed[i] <- mean(out$speed)
   ctmmSummary$maxSpeed[i] <- max(out$speed)
