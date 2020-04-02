198025645bd7dc2108c92af228a68290d0ec708f
Amanda Droghini <adroghini@alaska.edu>
2020-04-02 09:08:45 -0800

    Create 01-importDataSingleShapefile.R

---
+++ telemetry/scripts/01-importDataSingleShapefile.R
@@ -0,0 +1,8 @@
+# Objective: Import data from GPS collars. Data were provided as a single shapefile.
+
+# Data were downloaded by Kassie 30 March 2020.
+
+# Author: A. Droghini (adroghini@alaska.edu)
+#         Alaska Center for Conservation Science
+
+# Load packages and data files----
