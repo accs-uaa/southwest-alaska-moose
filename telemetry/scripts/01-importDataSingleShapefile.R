737b614d0b53d23aa4d4e18d751d5b8692d92832
Amanda Droghini <adroghini@alaska.edu>
2020-04-02 09:17:45 -0800

    Update 01-importDataSingleShapefile.R

--- telemetry/scripts/01-importDataSingleShapefile.R
+++ telemetry/scripts/01-importDataSingleShapefile.R
@@ -1,8 +1,14 @@
 # Objective: Import data from GPS collars. Data were provided as a single shapefile.
 
-# Data were downloaded by Kassie 30 March 2020.
+# Data were downloaded by Kassie on 31 March 2020.
 
 # Author: A. Droghini (adroghini@alaska.edu)
 #         Alaska Center for Conservation Science
 
 # Load packages and data files----
+library(sf)
+
+gpsData <- st_read("data/Moose_2020-03-31.shp")
+
+#### Export----
+save(gpsData, file="pipeline/01_importData/gpsRaw.Rdata")
