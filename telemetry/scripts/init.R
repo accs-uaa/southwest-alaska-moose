1f1894db1064c1c7f43a44d428ff57d8e98cddf5
Amanda Droghini <adroghini@alaska.edu>
2020-04-03 14:35:48 -0800

    Update init.R
    Added adehabitat and zoo packages

--- telemetry/scripts/init.R
+++ telemetry/scripts/init.R
@@ -5,7 +5,11 @@ library(tidyverse)
 # Spatial packages
 library(rgdal)
 
+# Time series packages
+library(zoo)
+
 # Animal movement packages
 library(move)
 library(ctmm)
 library(tlocoh) # for animating movement paths. Can also use moveVis but I was having problem aligning my data and I wanted something quick
+library(adehabitatLT)
