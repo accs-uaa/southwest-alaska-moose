eb9b4f2f0b645e69b4064251aec5649e686bcd00
Amanda Droghini <adroghini@alaska.edu>
2020-04-03 14:18:57 -0800

    Update 04b-exploreAnimatePaths.R
    Rewrote script to be based on data.frame, not move object. Some small parameter fixing.

--- telemetry/scripts/04b-exploreAnimatePaths.R
+++ telemetry/scripts/04b-exploreAnimatePaths.R
@@ -2,24 +2,33 @@
 
 # Load packages and data----
 rm(list=ls())
-library(tlocoh) # Can also use moveVis but I was having problem aligning my data
-load("pipeline/03c_interpolateData/mooseData.Rdata") 
+source("scripts/init.R")
+load("pipeline/03b_cleanLocations/cleanLocations.Rdata") 
 
-# Convert moveStak to locoh object
-data <- move.lxy(mooseData, use.utm = FALSE)
+# Create tlocoh object
+coords<-gpsClean[,9:10]
+data <- xyt.lxy(xy=coords,dt=gpsClean$datetime,proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"),id=gpsClean$deployment_id,dup.dt.check = TRUE)
+
+rm(coords)
 
 # Create animation----
 
 # Plotting parameters
 # Generate a color for each individual
+# Remove black and grays - hard to see dark shades in GE
+
 allColors <- colors(distinct = TRUE)
-allColors <- allColors[which(!grepl("gray",allColors))]
+allColors <- allColors[which(!grepl("gray|black",allColors))]
 set.seed(52)
 plotColors <- sample(allColors, 24)
 
 # Specify file path and name of individual kml layers
 filePath <- "pipeline/04b_animatePaths/temp/animateLocations"
-ids <- unique(mooseData@idData$deployment_id)
+ids <- unique(gpsClean$deployment_id)
 
 # Generate kml
-lxy.exp.kml(data, file=filePath, col=plotColors,id = ids, skip = 12, overwrite = TRUE, compress = FALSE, pt.scale = 0.3, show.path = FALSE, path.col = NULL, path.opaque = 80, path.lwd = 3)
+# Thin to one point per day
+lxy.exp.kml(data, file=filePath, col=plotColors,id = ids, skip = 12, overwrite = TRUE, compress = FALSE, pt.scale = 0.3, show.path = FALSE)
+
+# Clean workspace
+rm(list=ls())
