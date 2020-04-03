f754297e5d1e8de6772dae90c8b06a8e6cae236b
Amanda Droghini <adroghini@alaska.edu>
2020-04-03 14:18:13 -0800

    Update init.R
    Added tlocoh package

--- telemetry/scripts/init.R
+++ telemetry/scripts/init.R
@@ -8,3 +8,4 @@ library(rgdal)
 # Animal movement packages
 library(move)
 library(ctmm)
+library(tlocoh) # for animating movement paths. Can also use moveVis but I was having problem aligning my data and I wanted something quick
