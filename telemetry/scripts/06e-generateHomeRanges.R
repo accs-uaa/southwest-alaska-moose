d134b91d2d0be07aba45df42ea3124361f6bbb64
Amanda Droghini <adroghini@alaska.edu>
2020-06-03 22:28:37 -0800

    Update 06e-generateHomeRanges.R

--- telemetry/scripts/06e-generateHomeRanges.R
+++ telemetry/scripts/06e-generateHomeRanges.R
@@ -9,33 +9,60 @@ source("scripts/init.R")
 load("pipeline/06c_selectFinalModels/finalModels.Rdata")
 load("pipeline/06b_applyCalibration/calibratedData.Rdata")
 
+# Specify extent
+ee <- extent(calibratedData)
+
+# Split data into two chunks ----
+# Need to do this to avoid memory limit error
+# Work laptop has 16 GB of RAM, which is not enough even after increasing memory limit and trying to run on "fresh" (rebooted) computer
+
+# Order IDs alphabetically so that all home ranges for a given ID are contained in a single data chunk
+ids <- names(finalMods)
+ids <- ids[order(ids)]
+
+calibratedData <- calibratedData[ids]
+finalMods <- finalMods[ids]
+
+# names(finalMods) == ids # Check
+# names(calibratedData) == id
+
+# Create two datasets
+data1 <- calibratedData[1:14]
+mods1 <- finalMods[1:14]
+data2 <- calibratedData[15:34]
+mods2 <- finalMods[15:34]
+
+rm(calibratedData,finalMods,ids)
+
 #### Generate aKDE ----
 
 # Use weights = TRUE to account for gaps in data
+
+### Run first set
+gc() # Clear up memory
 Sys.time()
-homeRanges <- akde(data=calibratedData, CTMM=finalMods,
-                                     weights=TRUE)
+homeRanges02 <- akde(data=data2, CTMM=mods2,weights=TRUE,grid=ee)
 Sys.time()
 
-#### Explore results ----
-lapply(1:length(homeRanges), 
-       function(i) 
-        summary(homeRanges[[i]]))
+# Export homeRanges02
+save(homeRanges02,file="pipeline/06e_generateHomeRanges/homeRanges02.Rdata")
 
-# Plot
-lapply(1:length(homeRanges), 
-       function(i) 
-         plot(subsetData[[i]],UD=homeRanges[[i]],main=names(homeRanges)[[i]]))
+# Clear up memory
+rm(data2,mods2,homeRanges02)
+gc()
 
-#### Determining seasonal overlap
-overlap(homeRanges[1:2])
-overlap(homeRanges[4:5])
+homeRanges01 <- akde(data=data1, CTMM=mods1,
+                    weights=TRUE,grid=ee)
 
-# Export as shapefiles
-filePath <- paste(getwd(),"pipeline/05d_generateHomeRanges",sep="/")
+save(homeRanges01,file="pipeline/06e_generateHomeRanges/homeRanges01.Rdata")
 
-lapply(1:length(homeRanges), function (i) writeShapefile(homeRanges[[i]],
-               folder=filePath, file=names(homeRanges[i]),
-               level.UD=0.95))
+rm(data1,ee,mods1)
+
+# Merge home ranges back into one master file
+load("pipeline/06e_generateHomeRanges/homeRanges02.Rdata")
+homeRanges <- list(homeRanges01,homeRanges02)
+homeRanges <- flatten(homeRanges)
+
+save(homeRanges,file="pipeline/06e_generateHomeRanges/homeRanges.Rdata")
 
 rm(list=ls())
