4a5dc5f01fb1a7c4777fadf561fff93cedd87a1e
Amanda Droghini <adroghini@alaska.edu>
2020-06-06 23:11:31 -0800

    Update 06f-exportAsRasters.R
    Now (semi-) works for handling multiple .Rdata files, if home ranges are chunked up

--- scripts/06f-exportAsRasters.R
+++ scripts/06f-exportAsRasters.R
@@ -13,15 +13,16 @@ files <- list.files("pipeline/06e_generateHomeRanges",pattern="\\d+",full.names 
 
 filePath <- paste(getwd(),"pipeline/06f_exportAsRasters",sep="/")
 
-# Unfortunately lapply doesn't 
-# lapply(files,export_akdes,file_path=filePath)
+# I reach my memory limit after files 2/3 complete, so I ran the third file separately. In any case, this takes a while.
+# Issue: the function doesn't stop on its own, even after it creates all the rasters? 
+lapply(files,export_akdes,file_path=filePath)
+export_akdes(files,file_path=filePath)
 
 # level.UD doesn't give me anything different, regardless of value?
 
-
-
 # Normalize rasters using the Transformation / Normalize tool in: Evans JS, Oakleaf J, Cushman SA (2014) An ArcGIS Toolbox for Surface Gradient and Geomorphometric Modeling, version 2.0-0. URL: https://github.com/jeffreyevans/GradientMetrics Accessed: 2020 June 05.
 
+#### Export telemetry data points ----
 SpatialPointsDataFrame.telemetry(homeRanges)
 writeShapefile(homeRanges,
                folder=filePath, file=names(homeRanges),
