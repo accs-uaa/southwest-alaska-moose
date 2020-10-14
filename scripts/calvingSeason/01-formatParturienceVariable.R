bfc0283b77ca3990d60c717199277c90f9c21d71
Amanda Droghini <adroghini@alaska.edu>
2020-10-14 09:31:34 -0800

    Update 01-formatParturienceVariable.R

--- scripts/calvingSeason/01-formatParturienceVariable.R
+++ scripts/calvingSeason/01-formatParturienceVariable.R
@@ -8,7 +8,20 @@ library(readxl)
 
 calf2018 <- read_excel("data/calvingSeason/Parturience2018-2019.xlsx",sheet="2018",range="A1:AG67")
 
+load("pipeline/01_createDeployMetadata/deployMetadata.Rdata")
+
 # Format data----
-# Convert date to POSIX object 
 
 # Add collar ID using moose ID as a key
+calf2018 <- left_join(calf2018,deploy,by=c("Moose_ID"="animal_id"))
+
+# Convert date columns to long form
+calf2018 <- calf2018 %>% 
+  pivot_longer(cols="11 May 2018":"23 June 2018",names_to="date",values_to="calfStatus") %>% 
+  mutate(calfAlive = case_when(calfStatus > 1 ~ 1,
+                               calfStatus <= 1 ~ calfStatus)) %>% 
+  select(deployment_id,sensor_type,date,calfAlive)
+
+# Convert date to POSIX object 
+class(calf2018$date)
+calf2018$date <- as.POSIXct(calf2018$date,format="%e %B %Y")
