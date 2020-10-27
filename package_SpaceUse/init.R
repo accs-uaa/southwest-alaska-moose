1f0e4851faa162fcef289661bb4a542c4e0bcc01
Amanda Droghini <adroghini@alaska.edu>
2020-10-26 18:01:28 -0800

    Rename files

---
+++ package_SpaceUse/init.R
@@ -0,0 +1,19 @@
+# Data management packages
+library(plyr)
+library(tidyverse)
+library(lubridate)
+library(readxl)
+
+# Spatial packages
+library(raster)
+library(rgdal)
+library(sf)
+
+# Time series packages
+library(zoo)
+
+# Animal movement packages
+library(move)
+library(ctmm)
+library(tlocoh) # for animating movement paths. Can also use moveVis but I was having problem aligning my data and I wanted something quick
+library(adehabitatLT)
