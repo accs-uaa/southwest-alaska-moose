75429df983fdff00f3d0fd5bc83d9d5ce3151e0e
Amanda Droghini <adroghini@alaska.edu>
2020-10-26 18:03:53 -0800

    Update 01-formatParturienceVariable.R

--- data_Telemetry/01-formatParturienceVariable.R
+++ data_Telemetry/01-formatParturienceVariable.R
@@ -3,23 +3,22 @@
 # Author: A. Droghini (adroghini@alaska.edu)
 
 # Load packages and data----
-library(tidyverse)
-library(readxl)
+source("package_TelemetryFormatting/init.R")
 
 calf2018 <- read_excel("data/calvingSeason/Parturience2018-2019.xlsx",sheet="2018",range="A1:AG67")
 calf2019 <- read_excel("data/calvingSeason/Parturience2018-2019.xlsx",sheet="2019",range="A1:AH84")
 
-load("pipeline/01_createDeployMetadata/deployMetadata.Rdata")
+load(file="pipleine/telemetryData/gpsData/01_createDeployMetadata/deployMetadata.Rdata")
 
-# Format data----
+# Format data ----
 
 # Convert date columns to long form
 # For 2019, drop 1 bull moose from dataset
-calf2018 <- calf2018 %>% 
+calf2018 <- calf2018 %>%
   pivot_longer(cols="11 May 2018":"23 June 2018",names_to="date",values_to="calfStatus")
 
-calf2019 <- calf2019 %>% 
-  pivot_longer(cols="11 May 2019":"6 June 2019",names_to="date",values_to="calfStatus") %>% 
+calf2019 <- calf2019 %>%
+  pivot_longer(cols="11 May 2019":"6 June 2019",names_to="date",values_to="calfStatus") %>%
   filter(Notes!="Bull")
 
 # Combine both years into single data frame
@@ -29,12 +28,12 @@ calfData <- rbind(calf2018,calf2019)
 calfData <- left_join(calfData,deploy,by=c("Moose_ID"="animal_id"))
 
 # Create boolean calfAlive status
-calfData <- calfData %>% 
+calfData <- calfData %>%
   mutate(calfAlive = case_when(calfStatus > 1 ~ 1,
-                               calfStatus <= 1 ~ calfStatus)) %>% 
+                               calfStatus <= 1 ~ calfStatus)) %>%
   select(deployment_id,sensor_type,date,calfAlive)
 
-# Convert date to POSIX object 
+# Convert date to POSIX object
 calfData$date <- as.Date(calfData$date,format="%e %B %Y")
 
 #### Export data----
