322f203cf61ad880c601a760d283ebb7355cd6d2
Amanda Droghini <adroghini@alaska.edu>
2020-10-26 17:57:16 -0800

    File renaming
    To differentiate between GPS and VHF scripts.

--- data_Telemetry/02-formatVHF.R
+++ data_Telemetry/02-formatVHF.R
@@ -1,4 +1,4 @@
-# Objective: Format VHF data similar to GPS data so that both can be used in a habitat selection analysis.
+# Objective: Import VHF dataset & format it similar to GPS data so that both can be used in a habitat selection analysis.
 
 # Author: A. Droghini (adroghini@alaska.edu)
 
@@ -11,20 +11,20 @@ vhfData <- read_excel("data/calvingSeason/dbo_MooseRadioTelemetry.xlsx",sheet="d
 
 load("pipeline/01_createDeployMetadata/deployMetadata.Rdata")
 
-# Format data----
+# Format data ----
 
 # Join vhfData with deploy data to get collar ID (where applicable)
 # Add negative sign to longitude entries that do not have any.
 # Drop entries that do not have coordinates. some entries have a waypoint number but no coordinates- these cases need to be dropped as well (coordinates were lost)
 # Only include entries for which sensor_type = VHF. For GPS, no need for relocations since we will be using GPS collar data for those individuals. For sensor_type = "none", not enough observations for these individuals and no daily calf status since they couldn't be consistently relocated.
-vhfData <- left_join(vhfData,deploy,by=c("Moose_ID"="animal_id")) %>% 
+vhfData <- left_join(vhfData,deploy,by=c("Moose_ID"="animal_id")) %>%
   mutate(longX = case_when (Lon_DD < 0 ~ Lon_DD,
-                            Lon_DD > 0 ~ Lon_DD*-1)) %>% 
-  select(Moose_ID,deployment_id,sensor_type,Lat_DD,longX,FlightIndex_ID) %>% 
-  filter(!(is.na(Lat_DD) | is.na(longX))) %>% 
+                            Lon_DD > 0 ~ Lon_DD*-1)) %>%
+  select(Moose_ID,deployment_id,sensor_type,Lat_DD,longX,FlightIndex_ID) %>%
+  filter(!(is.na(Lat_DD) | is.na(longX))) %>%
   filter(sensor_type=="VHF")
 
-# Quality check ----
+# QA/QC ----
 
 # Check for coordinate outliers
 plot(vhfData$longX~vhfData$Lat_DD) # Examine two weird entries
@@ -33,7 +33,7 @@ which(vhfData$Lat_DD < 58.5)
 vhfData[1060,]
 vhfData[10,]
 # Delete both of those - 1060 is not in AK and 10 is in the Bering Sea
-vhfData <- vhfData %>% 
+vhfData <- vhfData %>%
   filter(!(Lat_DD < 58.5 | longX > -80))
 
 # Explore sample size
@@ -44,17 +44,18 @@ table(vhfData$deployment_id,vhfData$sensor_type)
 # Rename lat lon columns to match with GPS data formatting
 flightData <- select(.data=flightData,FlightIndex_ID,Flight_Date)
 
-vhfData <- left_join(vhfData,flightData,by="FlightIndex_ID") %>% 
-  select(-FlightIndex_ID) %>% 
+vhfData <- left_join(vhfData,flightData,by="FlightIndex_ID") %>%
+  select(-FlightIndex_ID) %>%
   rename(AKDT_Date = Flight_Date,latY = Lat_DD)
 
-# Convert date to as.Date object 
+# Convert date to as.Date object
 vhfData$AKDT_Date <- as.Date(vhfData$AKDT_Date,format="%Y-%m-%d")
 
 #### Export data----
-
 # As.Rdata file rather than .csv because I don't want to deal with reclassifying my dates
-save(vhfData,file="pipeline/calvingSeason/01_formatData/vhfData.Rdata")
+save(vhfData,file="pipeline/telemetryData/vhfData/vhfData.Rdata")
+
+write_csv(vhfData,file="output/cleanedVHFdata.csv")
 
 # Clean workspace
 rm(list=ls())
