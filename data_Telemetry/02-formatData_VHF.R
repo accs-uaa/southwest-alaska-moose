d6e4c608e496bcaf3d4423ea4169e1190f9fcb26
Amanda Droghini <adroghini@alaska.edu>
2020-10-27 09:59:37 -0800

    Update 02-formatData_VHF.R
    Exclude two additional outliers.

--- data_Telemetry/02-formatData_VHF.R
+++ data_Telemetry/02-formatData_VHF.R
@@ -8,7 +8,7 @@ source("package_TelemetryFormatting/init.R")
 flightData <- read_excel("data/calvingSeason/dbo_FlightIndex.xlsx",sheet="dbo_FlightIndex")
 vhfData <- read_excel("data/calvingSeason/dbo_MooseRadioTelemetry.xlsx",sheet="dbo_MooseRadioTelemetry")
 
-load("pipeline/01_createDeployMetadata/deployMetadata.Rdata")
+load("pipeline/telemetryData/gpsData/01_createDeployMetadata/deployMetadata.Rdata")
 
 # Format data ----
 
@@ -19,7 +19,7 @@ load("pipeline/01_createDeployMetadata/deployMetadata.Rdata")
 vhfData <- left_join(vhfData,deploy,by=c("Moose_ID"="animal_id")) %>%
   mutate(longX = case_when (Lon_DD < 0 ~ Lon_DD,
                             Lon_DD > 0 ~ Lon_DD*-1)) %>%
-  select(Moose_ID,deployment_id,sensor_type,Lat_DD,longX,FlightIndex_ID) %>%
+  dplyr::select(Moose_ID,deployment_id,sensor_type,Lat_DD,longX,FlightIndex_ID) %>%
   filter(!(is.na(Lat_DD) | is.na(longX))) %>%
   filter(sensor_type=="VHF")
 
@@ -32,8 +32,11 @@ which(vhfData$Lat_DD < 58.5)
 vhfData[1060,]
 vhfData[10,]
 # Delete both of those - 1060 is not in AK and 10 is in the Bering Sea
+# Mapping VHF data on our study area map revealed two additional outliers where longX > -156. VHF outliers easier to identify because we know the bounds of Kassie's flights.
 vhfData <- vhfData %>%
-  filter(!(Lat_DD < 58.5 | longX > -80))
+  filter(!(Lat_DD < 58.5 | longX > -156))
+
+plot(vhfData$longX~vhfData$Lat_DD) # Looks good.
 
 # Explore sample size
 table(vhfData$deployment_id,vhfData$sensor_type)
@@ -41,10 +44,10 @@ table(vhfData$deployment_id,vhfData$sensor_type)
 # Join with Flight Data ----
 # Join vhfData with flightData to obtain date of observation
 # Rename lat lon columns to match with GPS data formatting
-flightData <- select(.data=flightData,FlightIndex_ID,Flight_Date)
+flightData <- dplyr::select(.data=flightData,FlightIndex_ID,Flight_Date)
 
 vhfData <- left_join(vhfData,flightData,by="FlightIndex_ID") %>%
-  select(-FlightIndex_ID) %>%
+  dplyr::select(-FlightIndex_ID) %>%
   rename(AKDT_Date = Flight_Date,latY = Lat_DD)
 
 # Convert date to as.Date object
@@ -54,7 +57,7 @@ vhfData$AKDT_Date <- as.Date(vhfData$AKDT_Date,format="%Y-%m-%d")
 # As.Rdata file rather than .csv because I don't want to deal with reclassifying my dates
 save(vhfData,file="pipeline/telemetryData/vhfData/vhfData.Rdata")
 
-write_csv(vhfData,file="output/cleanedVHFdata.csv")
+write_csv(vhfData,"output/telemetryData/cleanedVHFdata.csv")
 
 # Clean workspace
 rm(list=ls())
