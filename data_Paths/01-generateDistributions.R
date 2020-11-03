536f1e3c96ad4b1d5c045212c04d9f2c2fca8075
Amanda Droghini <adroghini@alaska.edu>
2020-11-02 17:10:24 -0900

    Update 01-generateDistributions.R

--- data_Paths/01-generateDistributions.R
+++ data_Paths/01-generateDistributions.R
@@ -1,9 +1,10 @@
-# Objectives: Explore empirical distribution of step lengths and turning angles for GPS data. Generate random distribution based on theoretical distributions in the exponential family (gamma for step length, von Mises for turning angles).
+# Objectives: Explore empirical distribution of step lengths and turning angles for GPS data. Generate random distribution based on theoretical distributions: gamma for step length and von Mises/uniform for turning angles.
 
 # Code to generate theoretical distributions was adapted from the source code for the amt::distributions function (https://github.com/jmsigner/amt/)
 
 # Relevant literature:
 # 1) Forester JD, Im HK, Rathouz PJ. 2009. Accounting for animal movement in estimation of resource selection functions: Sampling and data analysis. Ecology 90:3554–3565.
+
 # 2) Signer J, Fieberg J, Avgar T. 2019. Animal movement tools (amt): R package for managing tracking data and conducting habitat selection analyses. Ecology and Evolution 9:880–890.
 
 # Author: A. Droghini (adroghini@alaska.edu)
@@ -30,6 +31,7 @@ gpsCalvingSeason$distance_meters <- unlist(lapply(move::distance(tracks), c, NA)
 hist(gpsCalvingSeason$bearing_degrees,
      main="Empirical distribution of bearings",
      xlab="Bearing (degrees)")
+# No evidence of directional persistence.
 
 hist(gpsCalvingSeason$distance_meters)
 hist(log(gpsCalvingSeason$distance_meters))
@@ -49,9 +51,15 @@ minDist <- min(distances[distances !=0])
 distances[distances == 0] <- minDist
 
 # Fit data to gamma distribution. Use lower argument to constrain estimated parameters to positive numbers only, as required by gamma distribution. Parameters estimated using MLE.
-fitGamma <- MASS::fitdistr(x = distances, densfun = "gamma", lower = c(0,0))
+fitGamma <- MASS::fitdistr(x = distances, 
+                           densfun = "gamma", lower = c(0,0))
 
-#### Von Mises distribution for angles ----
+#### Uniform distribution for angles ----
+# Use the Von Mises distribution (appropriate for circular data)
+# Requires two parameters: μ (mean = median = mode) and κ
+# If κ = 0, distribution is uniform
+
+# Check to see that our empirical data approximates a uniform distribution
 turnAngles<- (gpsCalvingSeason %>% 
                 filter(!is.na(bearing_degrees)) %>% 
                 dplyr::select(bearing_degrees))$bearing_degrees
@@ -63,22 +71,22 @@ fitVM <- circular::as.circular(turnAngles, type = "angles",
                            modulo = "asis", 
                            zero = 0, 
                            rotation = "counter")
-fitVMParams <- circular::mle.vonmises(fitVM)
-list(fitVMParams$mu, fitVMParams$kappa)
-# kappa  is close to zero - distribution is approx. uniform.
-# μ is a measure of location (the distribution is clustered around μ)
+# Get parameters
+circular::mle.vonmises(fitVM)
+# κ = 0.041 - distribution is approx. uniform.
 
 #### Generate random distances and angles ----
 randomDistances <- rgamma(n = 1e+06, shape=fitGamma$estimate[[1]], 
                           rate = fitGamma$estimate[[2]])
 
-# For random angles, amt sets mu to zero: make_distribution(name = "vonmises", params = list(kappa = kappa, mu = 0)). We copy this here, but either way our estimated μ is close to zero (mean = -0.731, SE = 0.2579)
+# For random angles, set κ = 0 to generate a uniform distribution
+# Set μ = 0, meaning left and right turns are equally likely. This is a property of the circular uniform distribution (https://en.wikipedia.org/wiki/Circular_uniform_distribution); also Avgar et al. 2016
 randomAngles <- circular::rvonmises(n=1e+06, mu=circular(0), 
-                          kappa=fitVMParams$kappa)
+                          kappa=0)
 
-# Convert from radians to degrees
+# Convert 'circular' structure to numeric vector
 randomAngles <- as.numeric(randomAngles) 
-hist(randomDegrees)
+hist(randomAngles)
 
 #### Export random numbers ----
 write_csv(as.data.frame(randomDistances), 
