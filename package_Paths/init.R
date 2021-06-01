b204a238f47d8ab53634e6a9c49ac0b8b0a88f37
Amanda Droghini <adroghini@alaska.edu>
2021-06-01 10:14:17 -0800

    Update file paths

--- package_Paths/init.R
+++ package_Paths/init.R
@@ -1,8 +1,10 @@
-# Define file directories
-input_dir <- "C:/Work/GMU_17_Moose/data_01_Input/"
-pipeline_dir <- "C:/Work/GMU_17_Moose/data_02_Pipeline/"
-output_dir <- "C:/Work/GMU_17_Moose/data_03_Output/"
-geoDB <- "C:/Work/GMU_17_Moose/gis/mooseHomeRanges.gdb"
+# Define directories
+drive <- "C:"
+root_folder <- "ACCS_Work/GMU_17_Moose"
+input_dir <- file.path(drive, root_folder, "Data_01_Input")
+pipeline_dir <- file.path(drive, root_folder, "Data_02_Pipeline")
+output_dir <- file.path(drive, root_folder, "Data_03_Output")
+geoDB <- file.path(drive, root_folder, "GIS/Moose_SouthwestAlaska.gdb")
 
 # Data management packages
 library(plyr)
