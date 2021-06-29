fd0d20d4785f37dd9ab741295a8494f1129e74f8
Amanda Droghini <adroghini@alaska.edu>
2021-06-28 20:55:15 -0800

    Update 02-generateConvexHulls.py
    Create single convex hull per moose (not per moose-year), buffer + clip to study area boundary

--- data_Paths/02-generateConvexHulls.py
+++ data_Paths/02-generateConvexHulls.py
@@ -1,4 +1,4 @@
-# Objectives: Create convex hull polygon for every individual moose for every year. These polygons will be used in our habitat selection analysis to define a bounding geometry within which to generate a random initial location for our random paths. Can also be used to compare home range estimates with other studies in Alaska.
+# Objectives: Create convex hull polygon for every moose using the entirety of that moose's telemetry data (all years, all seasons). These polygons will be used in our habitat selection analysis to define a bounding geometry within which to generate a random initial location for our random paths. Can also be used to compare home range estimates with other studies in Alaska.
 
 # Author: A. Droghini (adroghini@alaska.edu), Alaska Center for Conservation Science
 
@@ -20,22 +20,45 @@ arcpy.env.workspace = geodatabase # Needs to be set for Minimum Bounding Geometr
 # Define inputs
 input_projection = 3338
 input_csv = os.path.join(drive, root_folder, 'Data_03_Output\\animalData\\cleanedGPSdata.csv')
+study_area = os.path.join(geodatabase, "StudyArea_Boundary")
 x_coords = "Easting"
 y_coords = "Northing"
-unique_id = "mooseYear"
+unique_id = "deployment_id"
 
 # Define outputs
 output_layer = "telemetry_layer"
 output_shapefile = "cleanedGPSdata"
-output_polygon = os.path.join(geodatabase,"convexHulls")
+temp_convex = os.path.join(geodatabase,"convexHulls_temp")
+temp_convex_buffer = os.path.join(geodatabase, "convexHulls_buffer")
+output_convex = os.path.join(geodatabase, "convexHulls")
+buffer_dist = "2 Kilometers"
 
 # Define the initial projection
 initial_projection = arcpy.SpatialReference(input_projection)
 
 # Convert CSV to ESRI Shapefile
+print("Converting csv to shapefile...")
+
 arcpy.MakeXYEventLayer_management(input_csv, x_coords, y_coords, output_layer, spatial_reference=initial_projection)
 arcpy.conversion.FeatureClassToFeatureClass(in_features = output_layer, out_path = geodatabase, out_name = output_shapefile)
 
 # Create convex hull polygon for each moose
+print("Creating convex hull...")
 arcpy.MinimumBoundingGeometry_management(output_shapefile,
-                                         output_polygon, "CONVEX_HULL", group_option = "LIST", group_field = unique_id)
+                                         temp_convex, "CONVEX_HULL", group_option = "LIST", group_field = unique_id)
+
+# Buffer by 2 kilometers
+arcpy.analysis.Buffer(temp_convex, temp_convex_buffer, buffer_distance_or_field = buffer_dist)
+
+# Clip to study area boundary
+print("Clipping to study area...")
+arcpy.analysis.Clip(temp_convex_buffer, study_area, output_convex)
+
+# Remove temporary files
+files_Delete = [temp_convex,temp_convex_buffer]
+
+for i in files_Delete:
+    if arcpy.Exists(i):
+        arcpy.Delete_management(i)
+
+print("Creating convex hulls complete...")
